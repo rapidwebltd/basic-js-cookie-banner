73c73878a931d757cd374a805458a18ec5fff31a
Jordan Hall <jordan.hall@rapidweb.biz>
2016-04-14 10:12:36 +0100

    Ensure cookie-banner-dismiss path is set to site-wide ('/')

--- basic-js-cookie-banner.js
+++ basic-js-cookie-banner.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
-    
+
     if (document.cookie.indexOf('cookie-banner-dismiss')==-1) {
         $('.cookie-banner>.close').css('display', 'block');
     }
     else
     {
-        $('.cookie-banner').css('display', 'none');  
-    }
-    
-    $('.cookie-banner>.close').on('click', function() {
-        
         $('.cookie-banner').css('display', 'none');
-        
-        var setCookie = function(cname, cvalue, exdays) {
+    }
+
+    $('.cookie-banner>.close').on('click', function() {
+
+        $('.cookie-banner').css('display', 'none');
+
+        var setCookie = function(cname, cvalue, exdays, path) {
             var d = new Date();
             d.setTime(d.getTime() + (exdays*24*60*60*1000));
             var expires = "expires="+d.toUTCString();
-            document.cookie = cname + "=" + cvalue + "; " + expires;
+            document.cookie = cname + "=" + cvalue + "; " + expires + ";path=" + path;
         };
-        
-        setCookie('cookie-banner-dismiss', 1, 1825);
+
+        setCookie('cookie-banner-dismiss', 1, 1825, '/');
     });
-    
+
 });
